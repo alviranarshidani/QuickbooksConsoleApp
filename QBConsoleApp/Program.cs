fb4f15bc74ad2e730b1bb2e7af21ea2d74af2edf
Alvira Narshidani <Alvira.Narshidani@GeorgianCollege.ca>
2019-08-25 17:36:55 -0400

    Add project files.

---
+++ QBConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QBConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
